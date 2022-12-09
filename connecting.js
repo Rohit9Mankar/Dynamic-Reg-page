39c4fa0203b05ebba7e069b6663ac9572bec7b71
Rohit9Mankar <rohitmankar0079@gmail.com>
2022-12-09 15:53:04 +0530

    dom changes

--- connecting.js
+++ connecting.js
@@ -1,1 +1,7 @@
-console.log("anything");
+//console.dir(document)
+//console.log(document.doctype)
+const title=document.getElementById('header-title');
+title.style.borderBottom='solid 3px #000';
+
+const add_items=document.querySelector('.title');
+add_items.style.color='green'
