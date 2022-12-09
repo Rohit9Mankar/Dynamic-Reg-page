9744e6899d62641cca9e6b2ec14574066762094a
Rohit9Mankar <rohitmankar0079@gmail.com>
2022-12-09 16:26:41 +0530

    task3

---
+++ domTask3.js
@@ -0,0 +1,8 @@
+let items=document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
