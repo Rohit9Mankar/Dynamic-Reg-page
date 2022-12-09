1bd9b7c356d27907168fa82daf730f1ef66a8ec8
Rohit9Mankar <rohitmankar0079@gmail.com>
2022-12-09 16:44:34 +0530

    domtask5

---
+++ domTask5.js
@@ -0,0 +1,6 @@
+//Gives error
+//const item5=document.getElementsByClassName('list-group-item');
+//item5.style.textContent="By Class Name";
+
+const item=document.getElementsByTagName('li');
+item[4].textContent='By tag Name';
