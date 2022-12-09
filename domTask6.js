49b822d742b2bf16eb7183c7aabd5d2739feb8cf
Rohit9Mankar <rohitmankar0079@gmail.com>
2022-12-09 21:47:58 +0530

    domTask6thpart

---
+++ domTask6.js
@@ -0,0 +1,16 @@
+//This is part of !st deliverables
+//const item2=document.querySelector('.list-group-item:nth-child(2)');
+//item2.style.background='green'
+//item2.style.color='green'
+
+//const item3=document.querySelector('.list-group-item:nth-child(3)');
+//item3.style.display='none';
+
+//This below is part of second deliverables
+const odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++)
+{
+    
+        odd[i].style.background='green';
+    
+}
