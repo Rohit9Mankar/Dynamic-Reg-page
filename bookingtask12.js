3b9fb0505565efe273f828c55f52cc01d2104696
Rohit9Mankar <rohitmankar0079@gmail.com>
2022-12-11 17:12:58 +0530

    storing different users

---
+++ bookingtask12.js
@@ -0,0 +1,23 @@
+const form=document.getElementById('my_form');
+const itemslist=document.getElementById('users')
+
+form.addEventListener('submit',store_to_local)
+
+function store_to_local(e)
+{
+    e.preventDefault();
+   
+    const my_Obj={
+        name_h: document.getElementById('name').value,
+        contact : document.getElementById('email').value
+    };
+    var my_obj_stringed=JSON.stringify(my_Obj);
+    
+   localStorage.setItem('user detail'+JSON.stringify(my_Obj.name_h),my_obj_stringed);
+   
+   let li=document.createElement('li');
+   li.appendChild(document.createTextNode(localStorage.getItem('user detail'+JSON.stringify(my_Obj.name_h))))
+   itemslist.appendChild(li);
+
+
+}
